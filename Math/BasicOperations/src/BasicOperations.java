7de412ba58015ecdd077c1849795f6fd1a69f9b3
Reni Ferreira <renirogete.exp@gmail.com>
2025-01-06 21:53:11 -0300

    Subtraction class

---
+++ Math/BasicOperations/src/BasicOperations.java
@@ -0,0 +1,12 @@
+public class BasicOperations {
+    public static class addition {
+        public int execute(int a, int b) {
+            return a + b;
+        }
+    }
+    public static class Subtraction {
+        public int execute(int a, int b) {
+            return a - b;
+        }
+    }
+}
