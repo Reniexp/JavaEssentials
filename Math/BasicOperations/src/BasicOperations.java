8510f2e6405e252496ff088879d7b350e24b0830
Reni Ferreira <renirogete.exp@gmail.com>
2025-01-28 19:55:36 -0300

    Multiplication class

--- Math/BasicOperations/src/BasicOperations.java
+++ Math/BasicOperations/src/BasicOperations.java
@@ -9,4 +9,9 @@ public class BasicOperations {
             return a - b;
         }
     }
+    public static class Multiplication {
+        public int execute(int a, int b) {
+            return a * b;
+        }
+    }
 }
